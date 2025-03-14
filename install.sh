8d121cc46af07f97bbcf4583ea44480d8600b22d
iamGHOST755 <162158604+iamGHOST755@users.noreply.github.com>
2025-03-14 17:27:32 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash  
+
+clear
+
+echo "Press any key to continue. Ctrl + C to cancel."  
+read -n 1 -s -r # wait key press
+
+# check yay
+if command -v yay &> /dev/null; then  
+    echo "Yay found! Installing packages"
+    yay -S hyprland kitty ttf-jetbrains-mono-nerd dracula-rofi-git firefox rofi waybar thunar thunar-volman gvfs gvfs-mtp lxappearance nwg-look materia-gtk-theme papirus-icon-theme polkit-kde-agent sddm brightnessctl pavucontrol xdg-user-dirs hyprnotify fish kvantum kvantum qt5 kvantum-theme-materia bibata-cursor-theme-bin wlogout swaylock hyprshot playerctl pipewire pipewire-jack pipewire-alsa pipewire-audio pipewire-pulse wireplumber
+    cp -rf ~/hyprdots/config/* ~/.config
+    sudo systemctl enable sddm
+else  
+    echo "Yay not found. Installing yay"
+    cd ~
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si
+    echo "Yay installed! Installing packages"
+fi  
+
+yay -S hyprland kitty ttf-jetbrains-mono-nerd dracula-rofi-git firefox rofi waybar thunar thunar-volman gvfs gvfs-mtp lxappearance nwg-look materia-gtk-theme papirus-icon-theme polkit-kde-agent sddm brightnessctl pavucontrol xdg-user-dirs hyprnotify fish kvantum kvantum qt5 kvantum-theme-materia bibata-cursor-theme-bin wlogout swaylock hyprshot playerctl pipewire pipewire-jack pipewire-alsa pipewire-audio pipewire-pulse wireplumber
+cp -rf ~/hyprdots/config/* ~/.config
+sudo systemctl enable sddm
+xdg-user-dirs-update
+
+echo "Installation finished. Please reboot your system."
